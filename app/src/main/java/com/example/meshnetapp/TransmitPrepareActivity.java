c243577fc54635c64e1eb361583454992b02729a
friedroyce <friedroyze@gmail.com>
2020-05-17 17:43:33 +0800

    added transmission activities

---
+++ app/src/main/java/com/example/meshnetapp/TransmitPrepareActivity.java
@@ -0,0 +1,14 @@
+package com.example.meshnetapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TransmitPrepareActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_transmit_prepare);
+    }
+}
