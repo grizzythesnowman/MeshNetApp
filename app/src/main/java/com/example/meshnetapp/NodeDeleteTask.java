a9dc4653fb7530f70931bbc2f0e4c4d693f27922
friedroyce <friedroyze@gmail.com>
2020-05-17 23:01:08 +0800

    updated some variable names

--- app/src/main/java/com/example/meshnetapp/NodeDeleteTask.java
+++ app/src/main/java/com/example/meshnetapp/NodeDeleteTask.java
@@ -4,48 +4,34 @@ import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.os.AsyncTask;
-import android.view.View;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
-import android.widget.SimpleAdapter;
-import android.widget.TextView;
 import android.widget.Toast;
 
-
-import com.google.android.material.snackbar.Snackbar;
 
 import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.message.BasicNameValuePair;
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
 
 import org.apache.http.HttpResponse;
 import org.apache.http.NameValuePair;
 import org.apache.http.client.HttpClient;
-import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.DefaultHttpClient;
 
 import java.io.InputStream;
 import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Iterator;
 import java.util.List;
-import java.util.Map;
 
 import static android.app.Activity.RESULT_OK;
 
 
 public class NodeDeleteTask extends AsyncTask<String, String, String>{
-    String IP;
+    String NodeId;
     Context myContext;
     Activity myActivity;
 
-    public NodeDeleteTask(String ip, Context context, Activity activity) {
+    public NodeDeleteTask(String nodeId, Context context, Activity activity) {
         myContext = context;
         myActivity = activity;
-        IP = ip;
+        NodeId = nodeId;
     }
 
     protected void onPreExecute() {
@@ -70,7 +56,7 @@ public class NodeDeleteTask extends AsyncTask<String, String, String>{
         final String JSONResult;
         List<NameValuePair> DeviceInfo;
         DeviceInfo = new ArrayList<>(2);
-        DeviceInfo.add(new BasicNameValuePair("ip", IP.trim()));
+        DeviceInfo.add(new BasicNameValuePair("Id", NodeId.trim()));
 
         try {
             httpclient = new DefaultHttpClient();
