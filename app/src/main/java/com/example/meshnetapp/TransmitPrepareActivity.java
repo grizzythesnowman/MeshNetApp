403665317f5d5adc157391716c9f14c691b03d78
friedroyce <friedroyze@gmail.com>
2020-05-17 19:24:46 +0800

    added button function

--- app/src/main/java/com/example/meshnetapp/TransmitPrepareActivity.java
+++ app/src/main/java/com/example/meshnetapp/TransmitPrepareActivity.java
@@ -2,13 +2,26 @@ package com.example.meshnetapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class TransmitPrepareActivity extends AppCompatActivity {
+
+    Button Send;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_transmit_prepare);
+
+        Send.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent(TransmitPrepareActivity.this, TransmitResultActivity.class);
+                startActivityForResult(intent,1);
+            }
+        });
+
     }
 }
