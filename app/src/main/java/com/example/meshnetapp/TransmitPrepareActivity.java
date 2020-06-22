c5c64c2064621ae46d80ac63e1277318b3f1c439
sypatricia <sypatriciaaa@gmail.com>
2020-06-23 01:36:29 +0800

    updated main menu, transmit packets UI. also replaced backButton with Imageview backButton

--- app/src/main/java/com/example/meshnetapp/TransmitPrepareActivity.java
+++ app/src/main/java/com/example/meshnetapp/TransmitPrepareActivity.java
@@ -6,11 +6,12 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 
 public class TransmitPrepareActivity extends AppCompatActivity {
 
     Button btnSend;
-    Button btnBack;
+    ImageView backButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,7 +19,7 @@ public class TransmitPrepareActivity extends AppCompatActivity {
         setContentView(R.layout.activity_transmit_prepare);
 
         btnSend = findViewById(R.id.btnSend);
-        btnBack = findViewById(R.id.btnBack);
+        backButton = findViewById(R.id.backButton);
 
         btnSend.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
@@ -27,7 +28,7 @@ public class TransmitPrepareActivity extends AppCompatActivity {
             }
         });
 
-        btnBack.setOnClickListener(new View.OnClickListener() {
+        backButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 finish();
             }
